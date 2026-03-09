e32d5d17bcff9bfbf04dd869f687e420f5b89732
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-03-09 09:08:00 +0200

    Fourth Section first commit
    Added abasic rest controller

---
+++ pojoapp/src/main/java/com/azim/pojoapp/PojoappApplication.java
@@ -0,0 +1,13 @@
+package com.azim.pojoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojoappApplication.class, args);
+	}
+
+}
---
+++ pojoapp/src/test/java/com/azim/pojoapp/PojoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azim.pojoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
